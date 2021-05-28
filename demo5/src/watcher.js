6a0da6607117eb228aa5a905b6b6b2a183c09add
Xiaoxin <qwewsx789852@qq.com>
2021-05-28 19:52:12 +0800

    watcher 导入

---
+++ demo5/src/watcher.js
@@ -0,0 +1,9 @@
+/** Watcher 观察者,用于发射更新的行为 */
+class watcher {
+	/**
+	 *
+	 * @param {Object} vm JGVue 实例
+	 * @param {String | Function} expOrFn 如果时渲染watcher,传入的就是渲染函数,如果是 计算watcher 传入的就是路径表达式, 暂时只考虑expOrFn为函数的情况
+	 */
+	constructor(vm, expOrFn) {}
+}
